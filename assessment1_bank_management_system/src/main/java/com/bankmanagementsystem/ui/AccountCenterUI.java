062b623fcf17702bb33797e7aa4b186a6d3ee43c
soundar-19 <soundarrajacf@gmail.com>
2025-07-26 13:38:13 +0530

    feat : Introduced Separate class for Ui

---
+++ assessment1_bank_management_system/src/main/java/com/bankmanagementsystem/ui/AccountCenterUI.java
@@ -0,0 +1,15 @@
+package com.bankmanagementsystem.ui;
+
+public class AccountCenterUI {
+    
+    public static void show() {
+        System.out.println("\nWelcome to the Account Center");
+        System.out.println("Please select an option:");
+        System.out.println("1. Create Account");    
+        System.out.println("2. Delete Account");
+        System.out.println("3. Select Account");
+        System.out.println("4. Search Account");
+        System.out.println("5. Display All Accounts");
+        System.out.println("6. Exit");
+    }
+}
---
+++ assessment1_bank_management_system/src/main/java/com/bankmanagementsystem/ui/AccountDetailsUI.java
@@ -0,0 +1,15 @@
+package com.bankmanagementsystem.ui;
+
+public class AccountDetailsUI {
+    public static void show(){
+         System.out.println("\nSelect an option:");
+            System.out.println("1. Deposit Money");
+            System.out.println("2. Withdraw Money");
+            System.out.println("3. Balance Enquiry");
+            System.out.println("4. Display Account Details");
+            System.out.println("5. Print Transaction History");
+            System.out.println("6. Back to Account Center");
+            System.out.println("7. Exit");
+    }
+
+}
