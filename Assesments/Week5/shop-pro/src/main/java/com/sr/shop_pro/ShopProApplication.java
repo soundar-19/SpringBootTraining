7c924fb0d898013d0ba317d0d27ec2b8a1c86106
Soundar Raja B <soundarrajacf@gmail.com>
2025-08-22 11:56:14 +0530

    Feat : Initial Layout of Shop Pro

---
+++ Assesments/Week5/shop-pro/src/main/java/com/sr/shop_pro/ShopProApplication.java
@@ -0,0 +1,13 @@
+package com.sr.shop_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopProApplication.class, args);
+	}
+
+}
---
+++ Assesments/Week5/shop-pro/src/test/java/com/sr/shop_pro/ShopProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sr.shop_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
