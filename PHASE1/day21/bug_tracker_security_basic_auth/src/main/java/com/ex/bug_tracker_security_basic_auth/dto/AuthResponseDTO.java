b292a7ae465f9ac074053860edcf2e6ba0aef199
Soundar Raja B <soundarrajacf@gmail.com>
2025-08-22 10:45:00 +0530

    Day 21 - Swagger Ui / Open API Docs

---
+++ PHASE1/day21/bug_tracker_security_basic_auth/src/main/java/com/ex/bug_tracker_security_basic_auth/dto/AuthResponseDTO.java
@@ -0,0 +1,12 @@
+package com.ex.bug_tracker_security_basic_auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponseDTO {
+    private String token;
+}
